bee6df0d1b9882d72287497c7fbf660694a5a888
Nele02 <nele.magel@t-online.de>
2026-01-02 21:42:37 +0100

    feat(auth): setup auth routing, pages and basic ui components

---
+++ src/lib/ui/SplashScreen.svelte
@@ -0,0 +1,27 @@
+<section class="hero is-fullheight is-default is-bold">
+	<div class="hero-body">
+		<div class="container has-text-centered">
+			<div class="columns is-vcentered is-centered">
+				<div class="column is-8">
+					<h1 class="title is-2 mb-5">
+						<span class="icon mr-4"><i class="fas fa-mountain"></i></span>
+						PeakPoint
+					</h1>
+					<h2 class="subtitle is-4 mt-2">Track your peaks, categorize them, and plan your next hike.</h2>
+
+					<div class="buttons is-centered mt-5">
+						<a class="button is-medium is-success" href="/signup">
+							<span class="icon"><i class="fas fa-user-plus"></i></span>
+							<span>Sign Up</span>
+						</a>
+
+						<a class="button is-medium is-link" href="/login">
+							<span class="icon"><i class="fas fa-sign-in-alt"></i></span>
+							<span>Login</span>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
