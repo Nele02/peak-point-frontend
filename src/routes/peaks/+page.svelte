bc47fc9bb5589c1809ca1205a0f57f817e506c6f
Nele02 <nele.magel@t-online.de>
2026-01-02 22:00:59 +0100

    feat(auth): add layout and setup  auth routing

---
+++ src/routes/peaks/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { subTitle } from "$lib/runes.svelte";
+	import Card from '$lib/ui/Card.svelte';
+	subTitle.text = "Peaks";
+</script>
+
+
+<Card title="Peaks">
+	<p>Track Peaks here...</p>
+</Card>
--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -1,3 +1,14 @@
-<div class="notification is-warning">
-	Hello from signup!
-</div>
+<script lang="ts">
+	import SignupForm from "./SignupForm.svelte";
+</script>
+
+<section class="hero is-fullheight">
+	<div class="hero-body">
+		<div class="container">
+			<div class="column is-4 is-offset-4">
+				<h1 class="title has-text-centered">Signup to Peak Point</h1>
+				<SignupForm />
+			</div>
+		</div>
+	</div>
+</section>
