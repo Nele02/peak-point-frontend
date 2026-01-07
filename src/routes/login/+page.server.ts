b7d875c8383d2df6967ac85874563bae129a63bc
Nele02 <nele.magel@t-online.de>
2026-01-07 21:59:13 +0100

    feat(ssr): move login to server action

---
+++ src/routes/login/+page.server.ts
@@ -0,0 +1,32 @@
+import { dev } from "$app/environment";
+import { peakService } from "$lib/services/peak-service";
+import { redirect } from "@sveltejs/kit";
+
+export const actions = {
+	login: async ({ request, cookies }) => {
+		const form = await request.formData();
+		const email = form.get("email") as string;
+		const password = form.get("password") as string;
+
+		if (email === "" || password === "") {
+			throw redirect(307, "/login");
+		}
+
+		console.log(`attempting to log in email: ${email} with password: ${password}`);
+		const session = await peakService.login(email, password);
+
+		if (session) {
+			const userJson = JSON.stringify(session);
+			cookies.set("peak-user", userJson, {
+				path: "/",
+				httpOnly: true,
+				sameSite: "strict",
+				secure: !dev,
+				maxAge: 60 * 60 * 24 * 7
+			});
+			throw redirect(303, "/dashboard");
+		} else {
+			throw redirect(307, "/login");
+		}
+	}
+};
