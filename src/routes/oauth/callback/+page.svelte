c7ad8964df3dd8c6f4123a021405a3e9db2e30b8
Nele02 <nele.magel@t-online.de>
2026-01-09 22:50:11 +0100

    feat(oauth): implement github and google oauth

---
+++ src/routes/oauth/callback/+page.svelte
@@ -0,0 +1,8 @@
+<section class="hero is-fullheight">
+	<div class="hero-body">
+		<div class="container has-text-centered">
+			<p class="title">Logging you in...</p>
+			<p class="subtitle">Just a sec</p>
+		</div>
+	</div>
+</section>
