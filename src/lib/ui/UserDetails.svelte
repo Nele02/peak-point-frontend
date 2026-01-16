ab85a4f93b3757f7a9e7228ae9b714e92a124a8b
Nele02 <nele.magel@t-online.de>
2026-01-16 23:08:47 +0100

    feat (auth): validate signup input

--- src/lib/ui/UserDetails.svelte
+++ src/lib/ui/UserDetails.svelte
@@ -1,37 +1,36 @@
 <script lang="ts">
-  let { firstName = $bindable(""), lastName = $bindable("") } = $props();
+  const props = $props<{ values?: { firstName?: string; lastName?: string } }>();
+  const values = props.values ?? {};
 </script>
 
-<label class="label" for="firstName">Name</label>
-<div class="field is-horizontal">
-  <div class="field-body">
-    <div class="field">
-      <div class="control has-icons-left">
-        <input
-          bind:value={firstName}
-          class="input"
-          type="text"
-          placeholder="First Name"
-          name="firstName"
-        />
-        <span class="icon is-small is-left">
-          <i class="fa fa-id-card"></i>
-        </span>
-      </div>
-    </div>
-    <div class="field">
-      <div class="control has-icons-left">
-        <input
-          bind:value={lastName}
-          class="input"
-          type="text"
-          placeholder="Last Name"
-          name="lastName"
-        />
-        <span class="icon is-small is-left">
-          <i class="fa fa-id-card"></i>
-        </span>
-      </div>
-    </div>
+<div class="field">
+  <label class="label" for="firstName">First name</label>
+  <div class="control">
+    <input
+      id="firstName"
+      class="input"
+      type="text"
+      name="firstName"
+      placeholder="First name"
+      value={values.firstName ?? ""}
+      required
+      autocomplete="given-name"
+    />
+  </div>
+</div>
+
+<div class="field">
+  <label class="label" for="lastName">Last name</label>
+  <div class="control">
+    <input
+      id="lastName"
+      class="input"
+      type="text"
+      name="lastName"
+      placeholder="Last name"
+      value={values.lastName ?? ""}
+      required
+      autocomplete="family-name"
+    />
   </div>
 </div>
--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -1,15 +1,14 @@
 <script lang="ts">
   import SignupForm from "./SignupForm.svelte";
-
-  const props = $props();
+  export let form;
 </script>
 
 <section class="hero is-fullheight">
   <div class="hero-body">
     <div class="container">
-      <div class="column is-4 is-offset-4">
-        <h1 class="title has-text-centered">Signup to Peak Point</h1>
-        <SignupForm form={props.form} />
+      <div class="column is-5 is-offset-4">
+        <h3 class="title has-text-black has-text-centered">Create your account</h3>
+        <SignupForm {form} />
       </div>
     </div>
   </div>
